d288d04f3d77c8b15a26139ab081a1fc76cc541c
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-26 14:38:18 -0500

    new userlist component

---
+++ pages/userlist.js
@@ -0,0 +1,28 @@
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Layout from '../components/layout'
+import { Header, List } from 'semantic-ui-react'
+
+import { getUsers } from '../data/users'
+
+const Userlist = ( { users } ) => (
+  <Layout>
+    <Header as='h2'>Hello from Userlist!</Header>
+    <List>
+    {users.map(user => (
+      <List.Item key={user.firstName} content={user.firstName + ' ' + user.lastName} />
+    ))}
+    </List>
+  </Layout>
+)
+Userlist.getInitialProps = async function() {
+  const users = getUsers()
+  return { users }
+}
+Userlist.propTypes = {
+
+}
+
+export default Userlist
