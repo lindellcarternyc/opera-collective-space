3064ca3f99f6e6fc5851352d0b1b6275e28a618d
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-27 03:05:29 -0500

    updated mock users list and return promises from methods

--- data/users.js
+++ data/users.js
@@ -1,6 +1,48 @@
-let users = []
+let users = [
+  {
+    firstName: 'Lindell',
+    lastName: 'Carter',
+    email: 'lindellcarternyc@gmail.com',
+    password: 'password1',
+    admin: true
+  },
+  {
+    firstName: 'Jose',
+    lastName: 'Heredia',
+    email: 'joseheredia@gmail.com',
+    password: 'password2',
+    admin: true
+  },
 
-export const getUsers = () => users
+]
+
+export const getUsers = () => {
+  return new Promise((resolve, reject) => {
+    if (users.length > 0) {
+      resolve(users)
+    } else {
+      reject('NO USERS')
+    }
+  })
+}
+
+export const getUserByEmail = email => {
+  return new Promise((resolve, reject) => {
+      getUsers()
+        .then(allUsers => {
+          const usersWithEmail = allUsers.filter(u => u.email === email)
+          if (usersWithEmail.length === 1) {
+            const user = usersWithEmail[0]
+            resolve(user)
+          } else {
+            reject('SHOULD ONLY HAVE ONE USER WITH THAT EMAIL')
+          }
+        })
+        .catch(err => {
+          throw err
+        })
+  })
+}
 
 export const addUser = (user) => {
   users = [...users, user]
